2e39dbbfa29851170471648a3f1a327c6192926f
Gregory Pickart <gpickart88@gmail.com>
2017-03-06 14:30:55 -0500

    Valentines Day Sale over and scroll performance fix

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,22 +1,7 @@
 $(document).ready(function () {
     
-    /* Sticky Navigation */
-    $('.js--promotions').waypoint(function (direction) {
-        if (direction === 'down') {
-            $('nav').addClass('sticky');
-            $('hero').addClass('gone');
-            $('button').addClass('gone');
-        } else {
-            $('nav').removeClass('sticky');
-            $('hero').removeClass('gone');
-            $('button').removeClass('gone');
-        }
-    }, {
-        offset: '60px;'
-    });
-    
-    /* Sticky Navigation 
-    $('.js--who-we-are').waypoint(function (direction) {
+    /* Sticky Navigation With Promotions Section */
+    /* $('.js--promotions').waypoint(function (direction) {
         if (direction === 'down') {
             $('nav').addClass('sticky');
             $('hero').addClass('gone');
@@ -29,6 +14,21 @@ $(document).ready(function () {
     }, {
         offset: '60px;'
     }); */
+    
+    /* Sticky Navigation Without Promotions Section */
+    $('.js--who-we-are').waypoint(function (direction) {
+        if (direction === 'down') {
+            $('nav').addClass('sticky');
+            $('hero').addClass('gone');
+            $('button').addClass('gone');
+        } else {
+            $('nav').removeClass('sticky');
+            $('hero').removeClass('gone');
+            $('button').removeClass('gone');
+        }
+    }, {
+        offset: '110px;'
+    });
     
     /* Scroll */
     $('.js--logo').click(function () {
@@ -43,39 +43,5 @@ $(document).ready(function () {
     $('.js--contact-link').click(function () {
         $('html, body').animate({scrollTop: $('.js--contact-us').offset().top}, 1000);
     });
-    
-    /* Modal */
-    var modal = document.getElementById('myModal');
-
-    /* Get the image and insert it inside the modal - use its "alt" text as a caption */
-    var img1 = document.getElementById('firstImg');
-    var img2 = document.getElementById('secondImg');
-    var img3 = document.getElementById('thirdImg');
-    var img4 = document.getElementById('fourthImg');
-    var modalImg = document.getElementById("img01");
-    img1.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-    }
-    img2.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-    }
-    img3.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-    }
-    img4.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-    }
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("modal")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() { 
-      modal.style.display = "none";
-    }
     
 });
